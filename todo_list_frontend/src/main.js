99a8ebc9a707d0004f0731fbd4035f5f349e967c
Lucas Nunes <contato.lucasnunes2@gmail.com>
2025-08-20 20:35:06 -0300

    fix: login page

--- todo_list_frontend/src/main.js
+++ todo_list_frontend/src/main.js
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import './assets/main.css';
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
 
