bfbe0b7473fecb9943cc7f1fe662f928ef28a3d0
Lucas Nunes <contato.lucasnunes2@gmail.com>
2025-08-20 21:18:15 -0300

    fix: cors error

--- todo_list_frontend/src/main.js
+++ todo_list_frontend/src/main.js
@@ -8,7 +8,7 @@ import router from './router'
 import axios from 'axios'
 import './assets/main.css';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
+axios.defaults.baseURL = 'http://localhost:8000/api/'
 
 const app = createApp(App)
 
