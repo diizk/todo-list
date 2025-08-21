5a531d33168439da34ad9461d659c748008b03e8
Lucas Nunes <contato.lucasnunes2@gmail.com>
2025-08-20 21:17:57 -0300

    fix: cors error

---
+++ todo_list_backend/config/cors.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines which domains are allowed to access your
+    | application via JavaScript. You may pass a list of URLs or use a
+    | wildcard for all domains.
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
